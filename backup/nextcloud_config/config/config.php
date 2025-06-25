fdc007d45e197917475e7edd648406448e5c4c8a
Ubuntu <ubuntu@ip-172-31-0-183.eu-north-1.compute.internal>
2025-06-25 07:02:47 +0000

    Added Nextcloud + Nginx + Compose backup

---
+++ backup/nextcloud_config/config/config.php
@@ -0,0 +1,39 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'upgrade.disable-web' => true,
+  'instanceid' => 'ocwz29sz88mk',
+  'passwordsalt' => '4bxbRIT00VbiC66y7omRUt7cY0vrPt',
+  'secret' => 'HGUZew1Fi41hCcvRDJwPOUh22lPZTPCfhGE75avtGfreB8sr',
+  'trusted_domains' => 
+  array (
+    0 => 'cloud.hexmos.site',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'pgsql',
+  'version' => '31.0.6.2',
+  'overwrite.cli.url' => 'http://cloud.hexmos.site',
+  'dbname' => 'legaltech',
+  'dbhost' => 'db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_legaltech',
+  'dbpassword' => 'y7UZBictTYdbJ040O2nce9IJzyVsvx',
+  'installed' => true,
+);
